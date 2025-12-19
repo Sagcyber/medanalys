31d6652d2590b5d5cdac05ac1ca4a7526e6a172f
Ame Immortelle <blue.aquamarine@yandex.ru>
2025-12-19 11:51:09 +0700

    Add Test class

---
+++ src/main/java/com/medcenter/Test.java
@@ -0,0 +1,20 @@
+package com.medcenter;
+
+public class Test {
+
+    private String name;
+    private String description;
+
+    public Test(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
