02ef45568fa79a7f5f1cab169162afcafe67f345
Ame Immortelle <blue.aquamarine@yandex.ru>
2025-12-19 11:36:12 +0700

    Add Patient class with constructor and getters

--- src/main/java/com/medcenter/Patient.java
+++ src/main/java/com/medcenter/Patient.java
@@ -4,10 +4,25 @@ import java.util.List;
 
 public class Patient {
 
-    String name;
-    int age;
-    List<String> complains;
+    private String name;
+    private int age;
+    private List<String> complains;
 
+    public Patient(String name, int age, List<String> complains) {
+        this.name = name;
+        this.age = age;
+        this.complains = complains;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public int getAge() {
+        return age;
+    }
+
+    public List<String> getComplains() {
+        return complains;
+    }
 }
