a4f0729d052482c9610101743847b078f6a1caac
Ame Immortelle <blue.aquamarine@yandex.ru>
2025-12-21 14:33:58 +0700

    Add equals() and hashCode() to Test class to handle duplicates in Set

--- src/main/java/com/medcenter/Test.java
+++ src/main/java/com/medcenter/Test.java
@@ -1,9 +1,23 @@
 package com.medcenter;
 
+import java.util.Objects;
+
 public class Test {
 
     private String name;
     private String description;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Test test = (Test) o;
+        return Objects.equals(name, test.name) && Objects.equals(description, test.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description);
+    }
 
     public Test(String name, String description){
         this.name = name;
